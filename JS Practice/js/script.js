0efcd092cda1a6e1d7f3bc53371a6012af94402e
Tenisha <tenisha.grimmer@gmail.com>
2015-01-08 23:47:41 -0600

    Update indentation

--- JS Practice/js/script.js
+++ JS Practice/js/script.js
@@ -4,47 +4,47 @@ var columns = rows[0].cells.length;
 
 //Make each row clickable, and call createPopup with the row data for the row clicked.
 for(var i = 0; i < rows.length; i++) {
-  rows[i].addEventListener("click", function () {
-    createPopup(this);
-  });
+    rows[i].addEventListener("click", function () {
+        createPopup(this);
+    });
 }
 
 function createPopup(r) {
 
-    // Create an overlay div that will sit behind the popup so that a user cannot click
-    // anything until the popup is closed
-    var overlayDiv = document.createElement('div');
-    overlayDiv.id = 'overlay';
-    document.body.appendChild(overlayDiv);
+// Create an overlay div that will sit behind the popup so that a user cannot click
+// anything until the popup is closed
+var overlayDiv = document.createElement('div');
+overlayDiv.id = 'overlay';
+document.body.appendChild(overlayDiv);
 
-    //Create a popupDiv that will display a button to close itself and the results of the row
-    var popupDiv = document.createElement('div');
-    popupDiv.id = 'popup';
-    document.body.appendChild(popupDiv);
+//Create a popupDiv that will display a button to close itself and the results of the row
+var popupDiv = document.createElement('div');
+popupDiv.id = 'popup';
+document.body.appendChild(popupDiv);
 
-    document.getElementById('popup').style.display = 'block';
+document.getElementById('popup').style.display = 'block';
 
-    //Get the cell information from each cell of the table and add it to the popup div
-    var result = "";
+//Get the cell information from each cell of the table and add it to the popup div
+var result = "";
 
-    for(var i = 0; i < columns; i++){
-         var cellInfo = r.cells[i].innerHTML;
-         result += "Row: " + r.rowIndex + "  Column: " + i + ":  " + cellInfo + "<br><br>";
-    }
+for(var i = 0; i < columns; i++){
+    var cellInfo = r.cells[i].innerHTML;
+    result += "Row: " + r.rowIndex + "  Column: " + i + ":  " + cellInfo + "<br><br>";
+}
 
-    popupDiv.innerHTML = result;
+popupDiv.innerHTML = result;
 
-    //Append a button element to the popup div so that the user can close the div
-    var btn = document.createElement("BUTTON");
-    btn.id = 'button';
-    var t = document.createTextNode("Close");
-    btn.appendChild(t);
-    popupDiv.appendChild(btn);
+//Append a button element to the popup div so that the user can close the div
+var btn = document.createElement("BUTTON");
+btn.id = 'button';
+var t = document.createTextNode("Close");
+btn.appendChild(t);
+popupDiv.appendChild(btn);
 
-    //Remove the overlaydiv and the popupdiv after the close button has been clicked
-    btn.addEventListener("click", function() {
-      document.body.removeChild(overlayDiv);
-      document.body.removeChild(popupDiv);
-    });
+//Remove the overlaydiv and the popupdiv after the close button has been clicked
+btn.addEventListener("click", function() {
+    document.body.removeChild(overlayDiv);
+    document.body.removeChild(popupDiv);
+});
 
 }
